c8a46fc34c91a114708e6c0fe66b1066df56403a
Dante-in-Korea <71689654+Dante-in-Korea@users.noreply.github.com>
2020-12-22 09:36:12 +0900

    Create ec2.tf

---
+++ aws-terraform/module/ec2.tf
@@ -0,0 +1,94 @@
+resource "aws_security_group" "web_service" {
+    name = "terraform test"
+    vpc_id = "${aws_vpc.main.id}"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 0 
+        to_port =  0
+        protocol = "-1"
+        cidr_blocks = ["${var.public_subnets}"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "hardway-controller" { 
+    count = 3
+    ami = "ami-03461b78fdba0ff9d"
+    instance_type = "t3.small"
+    key_name = "test123"
+    vpc_security_group_ids = ["${aws_security_group.web_service.id}"]
+    subnet_id = aws_subnet.subnet.id
+
+    user_data = <<EOF
+            #! /bin/bash
+            hostnamectl set-hostname controller-${format(count.index)}
+    EOF
+
+    tags = {
+        Name = "Controller-${format(count.index)}"
+    }
+}
+
+resource "aws_instance" "hardway-worker" { 
+    count = 3
+    ami = "ami-03461b78fdba0ff9d"
+    instance_type = "t3.small"
+    key_name = "test123"
+    vpc_security_group_ids = ["${aws_security_group.web_service.id}"]
+    subnet_id = aws_subnet.subnet.id
+
+    user_data = <<EOF
+            #! /bin/bash
+            hostnamectl set-hostname worker-${format(count.index)}
+            pod-cidr=10.200.${format(count.index)}.0/24"
+    EOF
+
+    tags = {
+        Name = "Worker-${format(count.index)}"
+    }
+}
+
+resource "aws_instance" "hardway-haproxy" { 
+    ami = "ami-03461b78fdba0ff9d"
+    instance_type = "t3.small"
+    key_name = "test123"
+    vpc_security_group_ids = ["${aws_security_group.web_service.id}"]
+    subnet_id = aws_subnet.subnet.id
+
+    user_data = <<EOF
+            #! /bin/bash
+            hostnamectl set-hostname haproxy
+    EOF
+
+    tags = {
+        Name = "haproxy"
+    }
+}
+
+resource "aws_instance" "hardway-management" { 
+    ami = "ami-03461b78fdba0ff9d"
+    instance_type = "t3.small"
+    key_name = "test123"
+    vpc_security_group_ids = ["${aws_security_group.web_service.id}"]
+    subnet_id = aws_subnet.subnet.id
+
+    user_data = <<EOF
+            #! /bin/bash
+            hostnamectl set-hostname management
+    EOF
+
+    tags = {
+        Name = "management"
+    }
+}
