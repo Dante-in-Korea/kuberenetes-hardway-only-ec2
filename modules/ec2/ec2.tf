9c28c61f0f5dfd45e79ae8249e66b8c563212847
EC2 Default User <ec2-user@ip-192-168-100-36.ap-northeast-2.compute.internal>
2021-01-06 02:22:49 +0000

    init

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,27 +1,3 @@
-resource "aws_security_group" "web_service" {
-    name = "terraform test"
-    vpc_id = var.vpc_id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = 0
-        to_port =  0
-        protocol = "-1"
-        cidr_blocks = [var.cidr_block]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_instance" "hardway-controller" { 
     count = 3
     ami = var.ami
