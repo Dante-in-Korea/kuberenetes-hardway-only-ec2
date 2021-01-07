8f719c97d20efcf39e6e62a642834f17fbeaba57
EC2 Default User <ec2-user@ip-192-168-100-36.ap-northeast-2.compute.internal>
2021-01-07 04:37:03 +0000

    update

--- hardway/modules/ec2/sg.tf
+++ hardway/modules/ec2/sg.tf
@@ -4,7 +4,6 @@ resource "aws_security_group" "web_service" {
 }
 
 resource "aws_security_group_rule" "ssh" {
-<<<<<<< HEAD:modules/ec2/sg.tf
     from_port = 22
     to_port = 22
     protocol = "tcp"
@@ -29,30 +28,4 @@ resource "aws_security_group_rule" "outbound_vpc" {
     cidr_blocks = ["0.0.0.0/0"]
     type = "egress"
     security_group_id = aws_security_group.web_service.id
-=======
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "ssh"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_security_group_rule" "inbound_vpc" {
-    ingress {
-        from_port = 0
-        to_port =  0
-        protocol = "-1"
-        cidr_blocks = [var.cidr_block]
-    }
-}
-
-resource "aws_security_group_rule" "outbound_vpc" {
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
->>>>>>> main:hardway/modules/ec2/sg.tf
 }
