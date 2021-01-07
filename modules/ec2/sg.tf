3db36fa486a9fd60a03a9c905bc4e09992607ad0
EC2 Default User <ec2-user@ip-192-168-100-36.ap-northeast-2.compute.internal>
2021-01-07 04:37:12 +0000

    update

---
+++ modules/ec2/sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "web_service" {
+    name = "terraform test"
+    vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "ssh" {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    type = "ingress"
+    security_group_id = aws_security_group.web_service.id
+}
+
+resource "aws_security_group_rule" "inbound_vpc" {
+    from_port = 0
+    to_port =  0
+    protocol = "-1"
+    cidr_blocks = [var.cidr_block]
+    type = "ingress"
+    security_group_id = aws_security_group.web_service.id
+}
+
+resource "aws_security_group_rule" "outbound_vpc" {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    type = "egress"
+    security_group_id = aws_security_group.web_service.id
+}
